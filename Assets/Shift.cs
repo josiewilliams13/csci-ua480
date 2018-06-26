d6d25e4e5925fa5437865e372ae19b5544a9e31d
Josie <jvw242@nyu.edu>
2018-06-26 18:02:11 -0400

    Up-to-date

--- Assets/Shift.cs
+++ Assets/Shift.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Shift : MonoBehaviour
 {
     private Vector3 pos1 = new Vector3(0, 0, 0);
-    private Vector3 pos2 = new Vector3(0, 0, 0);
+    private Vector3 pos2 = new Vector3(1, 0, 0);
     public float speed = 1.0f;
 
     void Update()
