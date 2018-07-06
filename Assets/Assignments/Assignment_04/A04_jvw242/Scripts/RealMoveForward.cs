fa36bf75bc4b436178621ba5fddc2e70a10aaba3
Josie <jvw242@nyu.edu>
2018-07-05 22:15:09 -0400

    ;wq

---
+++ Assets/Assignments/Assignment_04/A04_jvw242/Scripts/RealMoveForward.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace A03_jvw242
+{
+
+    public class RealMoveForward : MonoBehaviour
+    {
+
+        public float speed;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetMouseButton(0))
+            {
+                //Debug.Log("MOUSE BYTTON PRESSED");
+                //speed * Time.deltaTime;
+                transform.Translate(Camera.main.transform.forward * speed * Time.deltaTime);
+            }
+        }
+    }
+}
