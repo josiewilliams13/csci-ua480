2edf1eb826497eadf9cd229e188465d34ef79aa4
dagoch <dagoch@gmail.com>
2018-07-22 18:55:06 -0400

    added networked multiplayer example in Assignment_07 folder

---
+++ Assets/Assignments/Assignment_07/_A07_Master/Scripts/NPCSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace A07Examples
+{
+    public class NPCSpawner : NetworkBehaviour
+    {
+
+        public GameObject npcPrefab;
+        public int numberOfNPCs;
+        public float maxDist = 7.0f;
+
+        public override void OnStartServer()
+        {
+            for (int i = 0; i < numberOfNPCs; i++)
+            {
+                var spawnPosition = new Vector3(
+                    Random.Range(-maxDist, maxDist),
+                    0.0f,
+                    Random.Range(-maxDist, maxDist));
+
+                var spawnRotation = Quaternion.Euler(
+                    0.0f,
+                    Random.Range(0, 180),
+                    0.0f);
+
+                var enemy = (GameObject)Instantiate(npcPrefab, spawnPosition, spawnRotation);
+                NetworkServer.Spawn(enemy);
+            }
+        }
+    }
+}
---
+++ Assets/Assignments/Assignment_07/_A07_Master/Scripts/RandomWalk.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace A07Examples
+{
+    public class RandomWalk : MonoBehaviour
+    {
+
+        Vector3 lastPosition;
+        Vector3 nextPosition;
+        public float speed;
+        bool walking = false;
+
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+            if (!walking)
+            {
+                walking = true;
+                Vector2 rand = Random.insideUnitCircle.normalized;
+                nextPosition = this.transform.position + new Vector3(rand.x, 0, rand.y);
+                lastPosition = this.transform.position;
+                StartCoroutine(Walk());
+            }
+        }
+
+        IEnumerator Walk()
+        {
+
+            float count = 0;
+            while (count < speed)
+            {
+                count += Time.deltaTime;
+                this.transform.position = Vector3.Lerp(lastPosition, nextPosition, count / speed);
+                yield return null;
+            }
+            walking = false;
+
+
+        }
+    }
+}
